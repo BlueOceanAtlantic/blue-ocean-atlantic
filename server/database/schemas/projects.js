81cb46113c236b527eba6d93cf18e3c364d1dfbc
Krista Brock <kb9286@gmail.com>
2021-04-03 17:48:01 -0500

    removing project_id

--- server/database/schemas/projects.js
+++ server/database/schemas/projects.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
 exports.projectsSchema = mongoose.Schema({
-  project_id: { type: Number, index: { unique: true } },
   project_name: String,
   project_description: String,
   project_owner: [{
